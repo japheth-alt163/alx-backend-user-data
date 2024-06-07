7ce770b175abad97e1145541d6d6f5b274bf2b99
japheth-alt163 <japhethbaraka4@gmail.com>
2024-06-07 08:27:23 +0000

    breakthrow

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,17 +4,22 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
